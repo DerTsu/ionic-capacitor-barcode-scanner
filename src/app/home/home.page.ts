59ff4815127323384e473763ca02f5ae424be70a
DerTsu <chingaroshy123@hotmail.com>
2024-04-29 14:08:28 -0500

    Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,7 @@ import { AlertController } from '@ionic/angular';
 export class HomePage implements OnInit{
   locations: { position: number, time: string, latitude: number, longitude: number }[] = [];
   duplicateLocationAlert: boolean = false;
+  isNewLocation: boolean = false;
 
   //B
   isSupported = false;
@@ -39,23 +40,45 @@ export class HomePage implements OnInit{
       };
       
       // Verificar si la nueva ubicación es diferente de las anteriores
+      /*
       const isNewLocation = !this.locations.some(location =>
         location.latitude === newLocation.latitude && location.longitude === newLocation.longitude
-      );
+      );*/
 
+      if (this.locations.length > 0) {
+        const distance = this.calculateDistance(
+        this.locations[this.locations.length - 1].latitude,
+        this.locations[this.locations.length - 1].longitude,
+        newLocation.latitude,
+        newLocation.longitude
+        );
+
+        if (distance < 1) {
+          this.toClose(distance);
+          return; // Salir de la función si la distancia es menor a 1 kilómetro
+        }
+
+        this.isNewLocation = true;
+
+      }else{
+
+        this.isNewLocation = true;
+
+      }
       
-      if (isNewLocation) {
+      if (this.isNewLocation) {
         console.log(newLocation)
         console.log("aceptada")
 
-        this.locations.push(newLocation);
-        this.newLocationAlert(newLocation);
-        this.saveLocations();
+        //Jaor
+        //this.locations.push(newLocation);
+        //this.newLocationAlert(newLocation);
+        //this.saveLocations();
 
         this.scan(newLocation);
 
         // Mostrar la alerta si la ubicación es duplicada
-        if (!isNewLocation && this.locations.length > 1) {
+        if (!this.isNewLocation && this.locations.length > 1) {
           this.showDuplicateLocationAlert(newLocation);
         }
       }else{
@@ -122,6 +145,23 @@ export class HomePage implements OnInit{
     window.open(url, '_blank');
   }
 
+  //DISNTANCIA
+  calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const R = 6371; // Radio de la Tierra en kilómetros
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distancia en kilómetros
+    return d;
+  }
+
+  deg2rad(deg: number) {
+    return deg * (Math.PI / 180);
+  }
 
   //B
 
@@ -133,10 +173,33 @@ export class HomePage implements OnInit{
       return;
     }
     const { barcodes } = await BarcodeScanner.scan();
-    this.barcodes.push(...barcodes);
+    
+    //this.barcodes.push(...barcodes);
     //this.locations.push(Locat);
     //this.newLocationAlert(Locat);
     //this.saveLocations();
+
+    if (barcodes.length > 0) {
+      const scannedData = barcodes[0].rawValue; // Obtener los datos del primer código escaneado
+
+      // Verificar si el código escaneado contiene el enlace específico
+      if (scannedData.toString() == 'https://www.youtube.com/') {
+
+        //Jafe
+        this.barcodes.push(...barcodes);
+        this.locations.push(Locat);
+        this.newLocationAlert(Locat);
+        this.saveLocations();
+
+      } else {
+          // El código escaneado no contiene el enlace específico
+          this.incorrectQR(scannedData);
+      }
+  } else {
+      // No se encontraron códigos QR
+      this.incorrectQR("Nada");
+  }
+
 
   }
 
@@ -150,6 +213,24 @@ export class HomePage implements OnInit{
     const alert = await this.alertController.create({
       header: 'Permission denied',
       message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async incorrectQR(parametro: any): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'QR inválido',
+      message: 'El QR escaneado no es válido. '+parametro,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async toClose(parametro: any): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Ya has escaneado un QR cerca de aquí. '+parametro+'Km',
       buttons: ['OK'],
     });
     await alert.present();
